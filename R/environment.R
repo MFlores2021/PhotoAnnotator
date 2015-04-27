f58644a77b0a7682678363e2a9fa3cb12d9711b3
Alexander Vowinkel <vowinkel.alexander@gmail.com>
2015-04-27 16:17:10 -0500

    modify shiny, copy old code

---
+++ R/environment.R
@@ -0,0 +1,25 @@
+
+pkg.env <- new.env()
+
+pkg.env$organisms <- 
+  c('plant',
+    'flower',
+    'flower dissection',
+    'fruit',
+    'seeds',
+    'tuber',
+    'root',
+    'sprout',
+    'herbarium',
+    'habitat',
+    'foliage',
+    'chips')
+
+source('AnnotatorTool.R')
+source('AnnotatorTool-seds.R')
+source('add.metadata.R')
+
+get_organisms <- function() {
+  pkg.env$organisms
+}
+  
